218404c5a4241011d91b7a3c2d8fa48003642b08
Bismar Trujillo <bismar20@hotmail.com>
2023-10-20 23:03:41 -0400

    delete method minor fix

--- src/Linked_List.java
+++ src/Linked_List.java
@@ -1,6 +1,5 @@
 public class Linked_List {
     private Node head;
-    private Node tail;
     private int size;
 
 
@@ -9,10 +8,21 @@ public class Linked_List {
         this.size = 0;
     }
     public void add(String word) {
-        Node node = new Node(word);
-        node.setNext(head);
-        head = node;
-        size++;
+        Node temp = head;
+
+        if (search(word) == 0){
+            Node node = new Node(word);
+            node.setNext(head);
+            head = node;
+            size++;
+        }else {
+            while(temp != null){
+                if(temp.getWord().equals(word)) {
+                    temp.setOccurrence(temp.getOccurrence() +1);
+                }
+                temp = temp.getNext();
+            }
+        }
     }
     public void display(){
         Node temp = head;
@@ -36,16 +46,22 @@ public class Linked_List {
         return occurrence;
     }
     public void delete(String word){
-        Node firstNode = head;
 
+        if(search(word) == 0) {
+            System.out.println(word + " not found");
+            return;
+        }
+        Node firstNode = head;
         while(head != null){
 
             if(head.getWord().equals(word)) { // checks first node for word
+                head.setOccurrence(head.getOccurrence()-1);
                 head = head.getNext();
                 firstNode = head;
                 size--;
-            } else if (head.getNext().getNext() == null) { //  conditions checks if we are second to last if true
-                head.setNext(null);                        // deletes last elemet
+            } else if (head.getNext().getNext() == null && head.getNext().getWord().equals(word)) {  // condition if second to last and last Node.word is "word"
+                head.setNext(null);   // deletes last element
+                head.setOccurrence(head.getOccurrence()-1);
                 size--;
             } else if (head.getNext().getWord().equals(word)){
                 head.setNext(head.getNext().getNext());
--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,20 @@ public class Main {
         Linked_List list1 = new Linked_List();
 
 
-        list1.add("first");
-        list1.add("second");
-        list1.add("third");
-        list1.add("forth");
-        list1.add("fifth");
-        list1.add("sixth");
-        list1.add("seventh");
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        list1.add("4");
+        list1.add("5");
+        list1.add("6");
+        list1.add("7");
+        list1.add("8");
+
 
         list1.display();
 
         System.out.println(list1.search("seventh"));
-        list1.delete("first");
+        list1.delete("00000000");
         list1.display();
 
 
